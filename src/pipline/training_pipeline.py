508108659e340203f93690ba2cb97635ed334f69
beniamine3155 <beniamine3155@gmail.com>
2025-09-16 12:58:49 +0600

    model evaluation and push in aws

--- src/pipline/training_pipeline.py
+++ src/pipline/training_pipeline.py
@@ -6,19 +6,27 @@ from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
+from src.components.model_evaluation import ModelEvaluation
+from src.components.model_pusher import ModelPusher
 
+from src.entity.config_entity import (  DataIngestionConfig,
+                                        DataValidationConfig,
+                                        DataTransformationConfig,
+                                        ModelTrainerConfig,
+                                        ModelTrainerConfig,
+                                        ModelEvaluationConfig,
+                                        ModelPusherConfig
+                                      )
 
-from src.entity.config_entity import DataIngestionConfig
-from src.entity.config_entity import DataValidationConfig 
-from src.entity.config_entity import DataTransformationConfig
-from src.entity.config_entity import ModelTrainerConfig
 
                                           
-from src.entity.artifact_entity import DataIngestionArtifact
-from src.entity.artifact_entity import DataValidationArtifact
-from src.entity.artifact_entity import DataTransformationArtifact
-from src.entity.artifact_entity import ModelTrainerArtifact
-
+from src.entity.artifact_entity import (DataIngestionArtifact,
+                                        DataValidationArtifact,
+                                        DataTransformationArtifact,
+                                        ModelTrainerArtifact,
+                                        ModelEvaluationArtifact,
+                                        ModelPusherArtifact
+                                        ) 
 
 
 class TrainPipeline:
@@ -27,6 +35,8 @@ class TrainPipeline:
         self.data_validation_config = DataValidationConfig()
         self.data_transformation_config = DataTransformationConfig()
         self.model_trainer_config = ModelTrainerConfig()
+        self.model_evaluation_config = ModelEvaluationConfig()
+        self.model_pusher_config = ModelPusherConfig()
         
 
 
@@ -101,6 +111,36 @@ class TrainPipeline:
         
 
 
+    def start_model_evaluation(self, data_ingestion_artifact: DataIngestionArtifact,
+                               model_trainer_artifact: ModelTrainerArtifact) -> ModelEvaluationArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting modle evaluation
+        """
+        try:
+            model_evaluation = ModelEvaluation(model_eval_config=self.model_evaluation_config,
+                                               data_ingestion_artifact=data_ingestion_artifact,
+                                               model_trainer_artifact=model_trainer_artifact)
+            model_evaluation_artifact = model_evaluation.initiate_model_evaluation()
+            return model_evaluation_artifact
+        except Exception as e:
+            raise MyException(e, sys)
+
+
+    def start_model_pusher(self, model_evaluation_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        """
+        This method of TrainPipeline class is responsible for starting model pushing
+        """
+        try:
+            model_pusher = ModelPusher(model_evaluation_artifact=model_evaluation_artifact,
+                                       model_pusher_config=self.model_pusher_config
+                                       )
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except Exception as e:
+            raise MyException(e, sys)
+
+
+
     def run_pipeline(self, ) -> None:
         """
         This method of TrainPipeline class is responsible for running complete pipeline
@@ -111,6 +151,12 @@ class TrainPipeline:
             data_transformation_artifact = self.start_data_transformation(
                 data_ingestion_artifact=data_ingestion_artifact, data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact=data_transformation_artifact)
+            model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                    model_trainer_artifact=model_trainer_artifact)
+            if not model_evaluation_artifact.is_model_accepted:
+                logging.info(f"Model not accepted.")
+                return None
+            model_pusher_artifact = self.start_model_pusher(model_evaluation_artifact=model_evaluation_artifact)
 
         except Exception as e:
             raise MyException(e, sys)
